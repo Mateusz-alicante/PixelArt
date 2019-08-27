64303d3f369f04b18d06f35b6d50d504f36bd943
Mateusz <mateusz.alicante@gmail.com>
2019-08-27 12:50:52 +0200

    test mobile support

--- client/src/Components/header/header.js
+++ client/src/Components/header/header.js
@@ -9,12 +9,12 @@ function Header () {
         <Aux>
             <div id="header">
                 <div id="header-image-container">
-                <img align="left" height="70" id="header-head-image" alt='header-head' src='pixel-art-header.png'></img>
-                <img align="right" height="70" id="spider" alt='header-spider' src='spider.png'></img>
+                <img className="images-header" align="left" height="70" id="header-head-image" alt='header-head' src='pixel-art-header.png'></img>
+                <img className="images-header" align="right" height="70" id="spider" alt='header-spider' src='spider.png'></img>
                 </div>
                 <h1 id='header-title-pixelart'>Pixel Art</h1>
             </div>
-            <Navigation />
+            <Navigation className="header-navigation-reference" />
         </Aux>
     )
 }
